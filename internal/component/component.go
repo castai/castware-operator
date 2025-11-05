534f5923773b15e4b1f3797dcb5af8e4a36b9f22
niccolocast <niccolo@cast.ai>
2025-11-05 11:38:30 +0100

    Validate extended permissions in webhook
    - Validate extended permissions in webhook
    - Set Helm value to support cluster controller

--- internal/component/component.go
+++ internal/component/component.go
@@ -3,14 +3,24 @@ package components
 import "github.com/samber/lo"
 
 const (
-	ComponentNameAgent    = "castai-agent"
-	ComponentNameOperator = "castware-operator"
+	ComponentNameAgent             = "castai-agent"
+	ComponentNameOperator          = "castware-operator"
+	ComponentNameClusterController = "cluster-controller"
 )
 
 func IsSupported(name string) bool {
 	// List of supported components
 	supportedComponents := []string{
 		ComponentNameAgent,
+		ComponentNameClusterController,
 	}
 	return lo.Contains(supportedComponents, name)
+}
+
+func RequiresExtendedPermissions(name string) bool {
+	// List of components requiring extended permissions
+	extendedPermissionsComponents := []string{
+		ComponentNameClusterController,
+	}
+	return lo.Contains(extendedPermissionsComponents, name)
 }
